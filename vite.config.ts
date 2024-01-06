6ef56e3b7a02635b10efb66ef5d5199a1f5b43a4
Farhan <animationfar@gmail.com>
2024-01-06 16:57:31 +0700

    feat: cache config to .cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
+import { type PluginOption, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
+import { writeFileSync, existsSync, mkdirSync } from "fs";
+import { inspect } from "util";
+
+if (!existsSync("./.cache")) mkdirSync("./.cache");
+
+const makeCache = (name: string, data: any) => writeFileSync(`./.cache/${new Date().getTime()}_${name}.js`, `(${ inspect(data, true, Infinity, false) })`);
+
+const the_plugin = (order: "post" | "pre") => ({
+  name: `the_cacher_${order}_${new Date().getTime()}`,
+  transformIndexHtml: {
+    order,
+    handler: (html, ctx) => {
+      makeCache(`transformIndexHtml_${order}`, { html, ctx });
+      return html;
+    },
+  },
+  transform: {
+    order,
+    handler(code, id, options) {
+      makeCache(`transform_${order}`, { code, id, options });
+      return {
+        code,
+      };
+    },
+  },
+} as PluginOption);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    the_plugin("pre"),
+    the_plugin("post"),
+  ],
 })
