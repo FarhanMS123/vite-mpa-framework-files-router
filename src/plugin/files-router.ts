155bd37383976aafb44a4821dc790bb2e2fd0be4
FarhanMS123 <animationfar@gmail.com>
2024-01-16 11:07:13 +0700

    feat: modify the traversFiles into plugin. STOP: I can't find a way to get the name of file.

--- src/plugin/files-router.ts
+++ src/plugin/files-router.ts
@@ -12,61 +12,19 @@ export const traversFiles = async ({ included, excluded, opts }: {
     opts?: GlobOptionsWithFileTypesUnset;
 }) => {
     included = included ?? defaultIncluded;
-    const files = await glob(pattern(excluded ?? defaultExcluded, included), {
-        nodir: true,
-        ...opts,
-    });
 
     const data: {
-        name: string;
-        filename: string;
-        entryFilename: string;
-        data: {
-            SCRIPT_SRC: string;
-        } & Record<string, unknown>;
-    }[] = [];
-    const input: UserConfig["build"]["rollupOptions"]["input"] = {};
-    const output: UserConfig["build"]["rollupOptions"]["output"] = [];
-
-    const cwd = process.cwd();
-
-    for (const filename of files) {
-        
-        // this would randomly rename the file to .html, for example:
-        // `a-file.page.tsx` and `a-file.page.vue` may just one to be rendered.
-        const rel = filename.slice(0, cwd.length) == cwd ? filename.slice(cwd.length + 1) : filename;
-        const rgExt = RegExp(`(${ included.join("|").replace(/\./ig, "\\.") })$`, "i");
-        const trExt = rel.slice(rel.search(rgExt) + 1).split(".");
-        let name = rel.replace(rgExt, "");
-
-        for (let i=trExt.length; i>=0; i--) {
-            const join = `${ i == trExt.length ? "" : "_" }${ trExt.slice(i).join("_") }`;
-            if (typeof input[`${ name }${ join }`] != "string") {
-                name += join;
-                break;
-            }
-        }
-
-        if (typeof input[name] == "string") continue;
-
-        if (rel.match(/\.html$/i)) input[name] = rel;
-        else if (typeof input[name] != "string") input[name] = "src/template/default.html";
-
-        output.push({
-            name,
-            entryFileNames: rel,
-        });
-        data.push({
-            name,
-            entryFilename: `${name}.html`,
-            filename: filename,
+        [name: string]: {
+            name: string;
+            filename: string;
+            entryFilename: string;
             data: {
-                SCRIPT_SRC: filename,
-            },
-        });
-    }
-
-    console.log(input, output, data);
+                SCRIPT_SRC: string;
+            } & Record<string, unknown>;
+        }
+    } = {};
+    let input: UserConfig["build"]["rollupOptions"]["input"] = {};
+    let output: UserConfig["build"]["rollupOptions"]["output"] = [];
 
     return {
         transformIndexHtml: {
@@ -76,9 +34,58 @@ export const traversFiles = async ({ included, excluded, opts }: {
                 tags: [],
             })
         },
-        config(config, env) {
-            return {
+        async config(config, env) {
+            input = {};
+            output = [];
+            const cwd = config.root ?? process.cwd();
+            
+            const files = await glob(pattern(excluded ?? defaultExcluded, included), {
+                cwd,
+                nodir: true,
+                ...opts,
+            });
 
+            for (const filename of files) {
+                // this would randomly rename the file to .html, for example:
+                // `a-file.page.tsx` and `a-file.page.vue` may just one to be rendered.
+                const rel = filename.slice(0, cwd.length) == cwd ? filename.slice(cwd.length + 1) : filename;
+                const rgExt = RegExp(`(${ included.join("|").replace(/\./ig, "\\.") })$`, "i");
+                const trExt = rel.slice(rel.search(rgExt) + 1).split(".");
+                let name = rel.replace(rgExt, "");
+
+                for (let i=trExt.length; i>=0; i--) {
+                    const join = `${ i == trExt.length ? "" : "_" }${ trExt.slice(i).join("_") }`;
+                    if (typeof input[`${ name }${ join }`] != "string") {
+                        name += join;
+                        break;
+                    }
+                }
+
+                if (typeof input[name] == "string") continue;
+
+                if (rel.match(/\.html$/i)) input[name] = rel;
+                else if (typeof input[name] != "string") input[name] = "src/template/default.html";
+
+                output.push({
+                    name,
+                    entryFileNames: rel,
+                });
+                data[name] = {
+                    name,
+                    entryFilename: `${name}.html`,
+                    filename: filename,
+                    data: {
+                        SCRIPT_SRC: filename,
+                    },
+                };
+            }
+
+            return {
+                build: {
+                    rollupOptions: {
+                        input, output,
+                    }
+                },
             };
         },
     } as PluginOption;
