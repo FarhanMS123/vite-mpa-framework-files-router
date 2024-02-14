f52889fa68d9bb5f73296fff3c6caed567890f4b
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-02-14 01:36:50 +0000

    unexpectedly, works

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import htmlTemplateMPA from 'vite-plugin-html-template-mpa';
+import virtualHtml from 'vite-plugin-virtual-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    htmlTemplateMPA({
-      minify: false,
-      pagesDir: process.cwd(),
+    virtualHtml({
       pages: {
-        page1: {
-          filename: "test/page1.html",
-          template: "src/clean.html",
-          inject: {
-            data: {
-              script_src: "test/main.tsx",
-            },
+        "test/hello.tsx": {
+          template: "/src/clean.html",
+          data: {
+            script_src: "/test/main.tsx"
           },
         },
       },
@@ -26,9 +21,6 @@ export default defineConfig({
 
   build: {
     rollupOptions: {
-      input: {
-        page1: "test/page1.html"
-      },
       external: /^(.git|.*\.local|dist|node_modules)$/ig,
     },
   },
