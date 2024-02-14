1267a4384100a547707b4c53e551781610290c12
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-02-14 04:04:59 +0000

    auto remove test

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     virtualHtml({
       pages: {
-        "dist/test/hello.tsx": {
+        "_/test/hello.tsx": {
           template: "/src/clean.html",
           data: {
             script_src: "/test/main.tsx"
