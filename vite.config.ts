73249757e2b36fbb110e44340c0889fa37f93c76
FarhanMS123 <animationfar@gmail.com>
2024-02-14 07:48:09 +0700

    feat: simple setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import htmlTemplateMPA from 'vite-plugin-html-template-mpa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    htmlTemplateMPA({
+      minify: false,
+      pagesDir: process.cwd(),
+      pages: {
+        page1: {
+          filename: "test/page1.html",
+          template: "src/clean.html",
+          inject: {
+            data: {
+              script_src: "test/main.tsx",
+            },
+          },
+        },
+      },
+    }),
+    splitVendorChunkPlugin(),
+    react(),
+  ],
+
+  build: {
+    rollupOptions: {
+      input: {
+        page1: "test/page1.html"
+      },
+      external: /^(.git|.*\.local|dist|node_modules)$/ig,
+    },
+  },
+
+  root: process.cwd(),
+  publicDir: false,
+  base: "/",
 })
