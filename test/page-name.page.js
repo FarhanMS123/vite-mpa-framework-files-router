d6230632850f638e9012d0cf2c69b29d8968b8fd
Farhan <animationfar@gmail.com>
2024-03-11 14:33:40 +0700

    fix: add creating element and dom in samples

--- test/page-name.page.js
+++ test/page-name.page.js
@@ -1,1 +1,3 @@
 // this is not empty
+
+console.log("hello");
--- test/page-name.page.tsx
+++ test/page-name.page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 export default function App() {
   return (<React.StrictMode>
@@ -13,3 +14,13 @@ export function Second() {
     </React.StrictMode>
   );
 }
+
+let root = document.getElementById("root")!;
+
+if (!root) {
+  root = document.createElement("div");
+  root.id = "root";
+  document.body.append(root);
+}
+
+ReactDOM.createRoot(root).render(<App />);
