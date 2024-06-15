85504aea3cc486274920f8823fb3d31e16fc5e0d
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-06-15 04:40:22 +0000

    feat!: plugins now works as workspace

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { PluginOption, defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { virtualRouter } from './src/plugin/files-router'
+import { virtualRouter } from './src/vite-virtual-router/files-router'
 import DynamicPublicDirectory from "./src/vite-multiple-assets"
-import { defaultExcluded } from './src/plugin/templates'
+import { defaultExcluded } from './src/vite-virtual-router/templates'
 import { showConfig } from './src/plugin/inspect'
 
 // https://vitejs.dev/config/
