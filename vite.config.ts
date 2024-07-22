2a1a5dac20b76262409dd5ba2e6d816876a00624
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-07-22 02:52:17 +0000

    feat: minimal plugin virtual

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,36 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
+import fs from "fs/promises"
+
+const prefix = "\0virtual:";
+const files = [
+  `${prefix}src/minimal.html`,
+  `${prefix}src/main_react.tsx`,
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    {
-      name: "",
-      
-    },
+    [
+      {
+        name: "virtual",
+        resolveId(source, importer, options) {
+          return source.slice(0, prefix.length) == prefix ? source.slice(prefix.length) : source;
+        },
+        async load(id, options) {
+          if (files.indexOf(`${prefix}${id}`) == -1) return;
+          let raw = await fs.readFile(id);
+          return raw;
+        },
+      } as PluginOption,
+    ],
     tsconfigPaths(),
     react(),
   ],
   build: {
     rollupOptions: {
-      input: [
-        "\0virtual:src/minimal.html",
-        "\0virtual:src/main_react.tsx",
-      ],
+      input: files,
     },
   },
 })
