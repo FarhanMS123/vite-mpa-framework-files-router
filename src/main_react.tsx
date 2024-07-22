4b439b5c4367e1d1782360cdecbf07f60598ef57
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-07-22 01:54:24 +0000

    feat: minimal reconstruction setup

---
+++ src/main_react.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createRoot } from '~/helper'
+
+/// @ts-ignore
+import App from "%SCRIPT_SRC%";
+
+const root = createRoot();
+
+/// @ts-ignore
+ReactDOM.createRoot(root).render(<App />);
