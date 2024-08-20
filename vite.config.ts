1044227fed57191bf1a2992f56dde0970c316967
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-08-20 12:21:59 +0000

    good

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from "fs/promises";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(async () => {
+  await fs.mkdir("./node_modules/.cache.vite-virtual", {
+    recursive: true,
+  });
+  await fs.copyFile("./sample.html", "./node_modules/.cache.vite-virtual/sample.html");
+
+  return {
+    build: {
+      rollupOptions: {
+        input: {
+          "home/app.html": "./sample.html",
+          "base.html": "./node_modules/.cache.vite-virtual/sample.html",
+        },
+      }
+    },
+    plugins: [react()],
+  };
 })
