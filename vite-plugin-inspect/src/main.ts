a70e80f5bc5e6849808477c975935acab74d13e2
FarhanMS123 <animationfar@gmail.com>
2025-08-16 11:35:19 +0700

    feat: simple function to log and flog

--- vite-plugin-inspect/src/main.ts
+++ vite-plugin-inspect/src/main.ts
@@ -1,10 +1,22 @@
-import { type PluginOption } from 'vite';
+import { ObjectHook as ObjectHook$1 } from 'rollup';
+import { type PluginOption, type Plugin } from 'vite';
 
-export type Plugin$1 = Exclude<NonNullable<Awaited<PluginOption>>, false | PluginOption[]>;
-export type ConfigKeys = keyof Plugin$1;
+export function log(name: string, args_encc: number[], args_plain: number[]) {
+    return (...params: unknown[]) => console.log(
+        name,
+        args_encc.map(ienc => encodeURIComponent(String(params[ienc]))),
+        args_plain.map(ienc => params[ienc]),
+    )
+}
+
+export type ObjectHook = Exclude<ObjectHook$1<false>, false>;
+
+export const flog = (handler: ObjectHook["handler"], order: ObjectHook["order"]) => ({ handler, order } as ObjectHook)
 
 // order, prop, func args, uriencode (for virtual module), non verbose
-export default function ViteInspect(opts: Map<ConfigKeys, (null | 'post' | 'pre')[]>, order: undefined | 'pre' | 'post') {
+export default function ViteInspect(opts: {
+    [k in keyof Plugin]: Plugin["apply"]
+}) {
     return [
         {},
     ] as PluginOption;
