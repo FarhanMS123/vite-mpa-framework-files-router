b8ea90d0e3b8cb2d43ca1f1ff3de320278536d3d
FarhanMS123 <animationfar@gmail.com>
2026-03-22 17:27:13 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kits-a1',
+  webDir: 'dist'
+};
+
+export default config;
