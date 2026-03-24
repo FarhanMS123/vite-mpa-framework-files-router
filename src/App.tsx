ff1b3348f2dd34f1419515b3be5fc362888fd933
FarhanMS123 <animationfar@gmail.com>
2026-03-24 15:56:42 +0700

    feat: difficult to use ionic. Need to go with capacitor.

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,14 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import NavBar from './components/NavBar';
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
+      <NavBar />
       <IonRouterOutlet>
         <Route exact path="/home">
           <Home />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,15 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <HomeContent />
       </IonContent>
     </IonPage>
   );
+};
+
+const HomeContent = () => {
+  return (<div>aaaa</div>)
 };
 
 export default Home;
